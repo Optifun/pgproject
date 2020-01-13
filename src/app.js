2c971254fee206150510b9feaeb754fd4a086de3
Optifun <frdima19@gmail.com>
2020-01-13 17:42:00 +0700

    Статика, главная страница, страница бронирования

--- src/app.js
+++ src/app.js
@@ -1,36 +1,57 @@
 require("dotenv").config();
-const {pugCompile} = require("../modules/pug");
 const express = require("express");
 const path = require("path");
 const projectPath = path.resolve(__dirname, "..");
 const app = express();
-const Cars = require("../modules/Cars");
+
+const { pugCompile } = require("../modules/pug");
+const RouteTypes = require("../modules/RouteTypes");
+const Routs = require("../modules/Routes");
+const EndPoints = require("../modules/EndPoints");
 
 //app.use(express.json);
 
+app.use(
+  "/static",
+  express.static(path.resolve(projectPath, "template", "public", "static"))
+);
 
-
-const load = () => {
-    let db = new Cars();
-    let data = db.cloneCars();
-    console.log("loaded " + data);
-    return data;
-}
-
-app.get("/index", (req, res) => {
-    let dataPromise = load();
-    dataPromise.then((incomeCars) => {
-        res.send(        
-            pugCompile(path.resolve(projectPath, "template", "index.pug"), {
-                grg: "Hello world!",
-                cars: incomeCars
-              })
-        );
-    })
+app.get("/", (req, res) => {
+  //let dataPromise = load();
+  let obj = pugCompile(path.resolve(projectPath, "template", "index.pug"));
+  console.log(obj);
+  res.send(obj);
 });
 
+app.get("/order", async (req, res) => {
+  //let dataPromise = load();
+  let routeTypes = await new RouteTypes().loadData();
+  let endPoints = await new EndPoints().loadData();
+  let startPoints = endPoints;
 
+  let obj = pugCompile(
+    path.resolve(projectPath, "template", "order_ticket.pug"),
+    {
+      data: {
+        startPoints,
+        endPoints,
+        routeTypes
+      }
+    }
+  );
+  res.send(obj);
+});
 
 app.listen(3000, () => {
-    console.log("Started!");
+  console.log("Started!");
 });
+/*
+  startPoints: [
+    { id: 1, name: "Moskow" },
+    { id: 4, name: "Saint Peter's burg" }
+  ],
+  endPoints: [
+    { id: 2, name: "Krasnoyarks" },
+    { id: 3, name: "Novosibirsk" }
+  ],
+  */
